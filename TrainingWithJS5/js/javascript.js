991540e2c8c0f57c07f276ebf7359bd8d247109f
Vitor Lima <mikomirOO@gmail.com>
2018-09-28 15:33:23 -0300

    Adding the 5th proj

---
+++ TrainingWithJS5/js/javascript.js
@@ -0,0 +1,12 @@
+// This now is a training with conditionals in JavaScript
+
+var a = 10;
+var b = 20;
+
+if( a < b ){
+    var result = a + b;
+    document.write("The result of a + b is : ",result);
+}
+
+// The command above is a conditional statement, that means it will only be read by the program if the codition is true
+// As the condition is true, this code will be interpreted and will be printed on screen the sentence -> "The result of a + b is : 30"
